38f40c0fb2d0b8085db349fdda4410668569144b
Drashti <drashtichunawala@gmail.com>
2024-11-27 20:08:18 +0530

    added middleware for authentication and added bcrypt for password hashing

--- server.js
+++ server.js
@@ -2,19 +2,32 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const bodyParser = require("body-parser");
-const personRoute=require('./routes/PersonRoutes')
-const menuRoute=require('./routes/MenuItemRoutes')
-const passport=require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const personRoute = require("./routes/PersonRoutes");
+const menuRoute = require("./routes/MenuItemRoutes");
+const passport = require("./auth");
 
 //to convert any incoming type of data into json format
 app.use(bodyParser.json()); //will save into req-body
+
+
+
+app.use(passport.initialize());
+const localAuthMiddleware =passport.authenticate("local",{session: false})
+
+
 app.get("/", function (req, res) {
-    res.send("Welcome");
+  res.send("Welcome to our hotel!");
 });
 
-app.use('/person', personRoute);
-app.use('/menu', menuRoute);
+//Middleware function
+const logRequest = (req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}]`);
+  next(); //move on to the next phase
+};
+app.use(logRequest);
+
+app.use("/person",   personRoute);
+app.use("/menu",localAuthMiddleware, menuRoute);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
